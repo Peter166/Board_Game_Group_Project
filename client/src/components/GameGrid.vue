58ed0b5f8b0990bc6bc07aa0db4c8ff1f90efc5a
user <user@users-MacBook-Pro.local>
2020-02-29 12:07:26 +0000

    adding css

--- client/src/components/GameGrid.vue
+++ client/src/components/GameGrid.vue
@@ -1,11 +1,15 @@
 <template lang="html">
   <div>
-    <hero-card class="herocards" v-for="(hero, index) in heroCards" :key="index" :hero="hero"/>
-    <dungeon-card class="dungeoncards" v-for="(dungeon, index) in dungeonCards" :key="index" :dungeon="dungeon"/>
 
-
-
-    <monster-card class="monstercards" v-for="(monster, index) in monsterCards" :key="index" :monster="monster"/>
+    <div class="container">
+      <div class="leftpane">
+        <h1>Player 1 + 3 Area</h1></div>
+      <div class="middlepane">Dungeon Area
+      <hero-card class="herocards" v-for="(hero, index) in heroCards" :key="index" :hero="hero"/>
+      <dungeon-card class="dungeoncards" v-for="(dungeon, index) in dungeonCards" :key="index" :dungeon="dungeon"/>
+      <monster-card class="monstercards" v-for="(monster, index) in monsterCards" :key="index" :monster="monster"/>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -49,13 +53,43 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+
+body, html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+}
+
+.leftpane {
+    width: 30%;
+    height: 100%;
+    float: left;
+    background-color: darkgrey;
+    background-size: cover;
+    border-right: black 3px;
+}
+
+.middlepane {
+    width: 70%;
+    height: 100%;
+    float: left;
+    background-image: url('https://ak9.picdn.net/shutterstock/videos/13298939/thumb/1.jpg');
+    border-collapse: collapse;
+}
+
 .herocards{
   /* background-color: darkred; */
   font-family: fantasy;
-  display: inline-grid;
-  padding-right: 5px;
-  padding-left: 5px;
-  /* border: 3px solid #8AC007; */
+  display:inline-flex;
+  padding-right: 3px;
+  padding-left: 3px;
+  /* border: 3px solid#8AC007; */
   text-align: center;
 
 }
@@ -74,8 +108,6 @@ export default {
   background-color: black;
   color: white;
   font-family: fantasy;
-
-  border: 3px solid black;
   text-align: center;
 }
 </style>
